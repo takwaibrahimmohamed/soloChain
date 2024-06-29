f70bc074b42dd1580c4aa005d833c873b995776d
takwaibrahimmohamed <takwaibrahimmohamed2002@gmail.com>
2024-06-29 18:28:09 +0300

    some ch

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// const navLinks=document.querySelectorAll(".navLinks li a")
+// navLinks.forEach((el)=>{
+//    el.addEventListener("click",(e)=>{
+//      console.log(e.target)
+//      e.target.classList.add("active")
+
+//    })
+// })
